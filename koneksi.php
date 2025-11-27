f0fdb4aa425ea89279ee3d66889201277dbba3cd
Al farabi <al.farabi.s@zohomail.com>
2025-11-27 22:10:08 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "ing_qna", null, "/opt/lampp/var/mysql/mysql.sock");
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
